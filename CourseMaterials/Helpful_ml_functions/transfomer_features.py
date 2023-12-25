33bef7caf0f06c8abbf785c4b00bdf86d0945427
Aglaril <jordanvardi@gmail.com>
2023-12-25 22:52:54 +0200

    Add transformer_features.py

--- CourseMaterials/Helpful_ml_functions/transfomer_features.py
+++ CourseMaterials/Helpful_ml_functions/transfomer_features.py
@@ -1,23 +1,25 @@
 def main():
-    
-def get_feature_names(column_transformer):
-    """Get feature names from a ColumnTransformer"""
-    output_features = []
+    def get_feature_names(column_transformer):
+        """Get feature names from a ColumnTransformer"""
+        output_features = []
 
-    for name, transformer, original_features in column_transformer.transformers_:
-        if name == 'remainder':  # Skip the 'remainder' transformer, if present
-            continue
+        for name, transformer, original_features in column_transformer.transformers_:
+            if name == "remainder":  # Skip the 'remainder' transformer, if present
+                continue
 
-        if hasattr(transformer, 'get_feature_names_out'):
-            # For transformers with a get_feature_names_out method (OneHotEncoder)
-            transformer_features = transformer.get_feature_names_out(original_features)
-        else:
-            # Transformers without get_feature_names_out (SimpleImputer)
-            transformer_features = original_features
+            if hasattr(transformer, "get_feature_names_out"):
+                # For transformers with a get_feature_names_out method (OneHotEncoder)
+                transformer_features = transformer.get_feature_names_out(
+                    original_features
+                )
+            else:
+                # Transformers without get_feature_names_out (SimpleImputer)
+                transformer_features = original_features
 
-        output_features.extend(transformer_features)
+            output_features.extend(transformer_features)
 
-    return output_features
+        return output_features
+
 
 if __name__ == "__main__":
     main()
