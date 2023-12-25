ea232cc8e50b1f191f6e4c0d78c7897e21815b62
Aglaril <jordanvardi@gmail.com>
2023-12-26 00:04:43 +0200

    Update Meeting_17_fun_exercise_jordan.ipynb

---
+++ CourseMaterials/Helpful_ml_functions/transformer_features.py
@@ -0,0 +1,33 @@
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.pipeline import Pipeline
+from sklearn.compose import ColumnTransformer
+
+
+def main():
+    pass
+
+
+def get_feature_names(column_transformer):
+    """Get feature names from a ColumnTransformer"""
+    output_features = []
+
+    for name, transformer, original_features in column_transformer.transformers_:
+        if name == "remainder":  # Skip the 'remainder' transformer, if present
+            continue
+
+        if hasattr(transformer, "get_feature_names_out"):
+            # For transformers with a get_feature_names_out method (OneHotEncoder)
+            transformer_features = transformer.get_feature_names_out(original_features)
+        else:
+            # Transformers without get_feature_names_out (SimpleImputer)
+            transformer_features = original_features
+
+        output_features.extend(transformer_features)
+
+    return output_features
+
+
+if __name__ == "__main__":
+    main()
